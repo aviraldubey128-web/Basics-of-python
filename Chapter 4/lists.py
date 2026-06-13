9ddc1d22f6b183b573a69f8ddf2143935abbced6
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 17:06:42 +0530

    Update lists.py

--- Chapter 4/lists.py
+++ Chapter 4/lists.py
@@ -4,3 +4,7 @@ items[1] = "Pineapple" # this will change the item at index 1 from "Banana" to "
 print(items) 
 
 print(items[1:6])
+
+# Output -> Bnnana
+# output ->['Apple', 'Pineapple', 'Cherry', 'Date', 'Elderberry']
+# output -> ['Pineapple', 'Cherry', 'Date', 'Elderberry']
