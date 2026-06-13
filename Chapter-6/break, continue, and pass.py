2a5c3915281ef8defacb6e33b9fb300f1f578386
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 20:22:50 +0530

    Refactor loop examples with outputs
    Updated examples for break, continue, and pass statements with output.

--- Chapter-6/break, continue, and pass.py
+++ Chapter-6/break, continue, and pass.py
@@ -7,15 +7,32 @@ for i in range(1, 13):
         break
     i += 1
 
+output =''' iteration 1
+iteration 2
+iteration 3
+iteration 4
+iteration 5
+iteration 6
+iteration 7
+iteration 8
+iteration 9
+iteration 10
+iteration 11
+iteration 12'''
+
 # continue the loop
 
 i = 1
 for i in range(1, 6):
-    if i == 4:
+    if i == 1:
         continue
     print("iteration", i)
     i +=1
-
+output = '''iteration 2
+iteration 3
+iteration 4
+iteration 5
+'''
 # pass the loop
 
 i = 1
@@ -25,5 +42,5 @@ for i in range(1,6):
 print("iteration", i)
 i+=1
 
-
+output = '''iteration 9''''
 
