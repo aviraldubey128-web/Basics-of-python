5293005c60dd2dd8da5b98607ef1453d4565b658
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 20:25:26 +0530

    Update Introduction to Functions.py

--- chapter-7/Introduction to Functions.py
+++ chapter-7/Introduction to Functions.py
@@ -9,15 +9,26 @@ f = int(input("Enter the sixth number:"))
 # This wil certainly reduce the readablity of code 
 # to avoid this we use parameter and arugment
 
-
+output = Enter the first number:123
+Enter the second number:322
+Enter the third number:212 
+Enter the fourth number:322
+Enter the fifth number:212
+Enter the sixth number:322
 def sum():
     print("THE SUM OF a & b", a+b)
 sum()
+
+output = '''THE SUM OF a & b 445'''
+
 def product():
     print("THE PRODUCT OF c & d", c*d)
 product()
+
+output = '''THE PRODUCT OF c & d 68264'''
 
 def division():
     print("THE DIVISION OF e AND f", e/f)
 division()
 
+output = '''THE DIVISION OF e AND f 0.6583850931677019'''
