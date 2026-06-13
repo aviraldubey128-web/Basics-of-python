befad87352bee81db1ecd724b2d7c2124c30ef17
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 20:40:00 +0530

    Update method_function_inside_class.py

--- chapter-9/method_function_inside_class.py
+++ chapter-9/method_function_inside_class.py
@@ -1,14 +1,1 @@
-class student:
-    def __init__(self, name):
-        self.name = name
-        print(self.name)
-s1 = student("aviral")
-s2= student("khushi")
 
-class student:
-    def __init__(self, name):
-        self.name = name
-    def hello(self):
-        print("hello",self.name)
-s1 = student("aviral")
-s1.hello()
