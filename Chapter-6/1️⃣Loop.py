9687fabeccd772e4b731199c9113deaaa526d2a2
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 20:10:26 +0530

    Update 1️⃣Loop.py

--- Chapter-6/1️⃣Loop.py
+++ Chapter-6/1️⃣Loop.py
@@ -3,5 +3,63 @@ while i <=3:
      print("hello")
      i +=1
 
+
 for f in range(3):
     print("Hello")
+
+output = '''hello
+         hello
+         hello
+         Hello
+         Hello
+         Hello'''
+
+for i in range(90,100):
+    print("iteration", i)
+output = '''
+iteration 90
+iteration 91
+iteration 92
+iteration 93
+iteration 94
+iteration 95
+iteration 96
+iteration 97
+iteration 98
+iteration 99
+            '''        
+
+     items = ["pizza", "samosa", "kachori", "gulab jamun"]
+for item in items:
+    print("I like to eat", item)
+
+output = '''I like to eat pizza
+I like to eat samosa
+I like to eat kachori
+I like to eat gulab jamun '''
+
+for i in range(1, 5):
+    for j in range(2,7):
+        print("interation", i, j)
+        j+=1
+output = '''
+interation 1 2
+interation 1 3
+interation 1 4
+interation 1 5
+interation 1 6
+interation 2 2
+interation 2 3
+interation 2 4
+interation 2 5
+interation 2 6
+interation 3 2
+interation 3 3
+interation 3 4
+interation 3 5
+interation 3 6
+interation 4 2
+interation 4 3
+interation 4 4
+interation 4 5
+interation 4 6'''
