0c3effca38dd3ac387372f0a18ad25ad31af8c1d
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 16:49:44 +0530

    Update escape.py

--- Chapter-3/escape.py
+++ Chapter-3/escape.py
@@ -3,11 +3,12 @@ string = "This morning is very \"lazy\" \nBut it's time to wake up \tand have to
 print(string)
 # This morning is very "lazy" 
 # But it's time to wake up        and have to go to work 
-\n -> New line
-\t -> Tab space
-\\ -> Backslash
-\' -> single quote
-\" -> double quote 
-\r ->carriage return
-\b -> backspace 
-\a -> alert/bell
+
+# \n -> New line
+# \t -> Tab space
+# \\ -> Backslash
+# \' -> single quote
+# \" -> double quote 
+# \r ->carriage return
+# \b -> backspace 
+# \a -> alert/bell
