ea1ff90eb9490b5417d74c55ffd522b9abc70cdc
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 17:37:33 +0530

    Update tuple method.py

--- Chapter 4/tuple method.py
+++ Chapter 4/tuple method.py
@@ -1,4 +1,25 @@
-a = (2 , 2, 3 ,1, 2 , 33)
+### METHODS OF TUPLES
 
-b = a.count(2)
-print(b)
+a = (2 , 2, 3 ,1, -2 , 6)
+
+print(a.count(2))
+# 2
+print(a.index(-2))
+# 4
+print(a.index(-2,3))
+# 4
+print(len(a))
+# 6
+print(max(a))
+# 6
+print(min(a))
+# -2
+print(sum(a))
+# 12
+print(sorted(a))
+# [-2, 1, 2, 2, 3, 6]
+
+
+
+
+
