3807d6edf6f952108d13500a6afa4c1d26c9fd72
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 16:54:16 +0530

    Update negative string.py

--- Chapter-3/negative string.py
+++ Chapter-3/negative string.py
@@ -11,3 +11,10 @@ print(nameshort2)
 print(name[:4]) # start from index 0 and end at index 4 (exclusing 4)
 print(name[1:]) # start from index 1 and the length of the string (exclusing the length)
 print(name[1:6]) # start from index 1 and end at index 6 (exclusing 6
+
+# avi
+# ira
+# ira
+# avir
+# viral
+# viral
