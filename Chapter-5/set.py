682468182fb5d91b42b9e8fe23222fd62a9b3963
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 20:00:59 +0530

    Update set.py

--- Chapter-5/set.py
+++ Chapter-5/set.py
@@ -2,4 +2,30 @@ elt = {"apples", "bananas", "cherries", "apples", "cherries"}
 print(type(elt))
 
 print(elt)
+<class 'set'>
+{'cherries', 'apples', 'bananas'}
 
+## Set methods:
+
+elt = {"apples", "bananas", "graphs", "apples", "graphs"}
+elt2 = {"grapes", "apples", "pears", "grapes", "pears"}
+
+elt.add("5")
+print(elt)
+
+output = {'5', 'apples', 'bananas', 'graphs'}
+
+elt.union(elt2)
+print(elt.union(elt2))
+
+output = {'apples', 'graphs', '5', 'grapes', 'pears', 'bananas'}
+
+elt.intersection(elt2)
+print(elt.intersection(elt2))
+
+output = {'apples'}
+
+elt.remove("cherries")
+print(elt)
+
+output = {'5', 'apples', 'graphs'}
